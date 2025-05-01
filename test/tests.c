993e52da2e8afe2bbf620f24e82a8a559cc8a0c8
Fernando Dodino <fernando.dodino@gmail.com>
2025-05-01 10:29:28 -0300

    Change tests using factory methods

--- test/tests.c
+++ test/tests.c
@@ -6,66 +6,98 @@
 context (test_de_alumnos) {
   
   describe("Tests de Alumno") {
-    Alumno * nico;
-    Alumno * alumnoEstudioso;
-    Alumno * alumnoHijoDelRigor;
-    Alumno * alumnoCabulero;
-    Parcial * parcialConLongitudPar;
-    Parcial * parcialConLongitudImpar;
-    Parcial * parcialConMuchasPreguntas;
-    Parcial * parcialConPocasPreguntas;
+    Alumno * crearAlumno() {
+      return Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, estudioso);
+    }
 
-    before {
-      nico = Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, estudioso);
-      alumnoEstudioso = Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, estudioso);
-      alumnoHijoDelRigor = Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, hijoDelRigor);
-      alumnoCabulero = Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, cabulero);
-      parcialConLongitudPar = Parcial_new("Paradigmas de programacion", 5);
-      parcialConLongitudImpar = Parcial_new("Sistemas Operativos", 8);
-      parcialConMuchasPreguntas = Parcial_new("Paradigmas de programacion", 6);
-      parcialConPocasPreguntas = Parcial_new("Sistemas Operativos", 4);
-    } end
+    Alumno * crearAlumnoEstudioso() {
+      return Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, estudioso);
+    }
 
-    after {
-      FREE(nico);
-      FREE(alumnoEstudioso);
-      FREE(alumnoHijoDelRigor);
-      FREE(alumnoCabulero);
-      FREE(parcialConLongitudPar);
-      FREE(parcialConLongitudImpar);
-    } end
+    Alumno * crearAlumnoHijoDelRigor() {
+      return Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, hijoDelRigor);
+    }
+
+    Alumno * crearAlumnoCabulero() {
+      return Alumno_new("Nico", "Gomez", "Av. siempre viva 742", 20, 111111, cabulero);
+    }
+
+    Parcial * crearParcialConLongitudPar() {
+      return Parcial_new("Paradigmas de programacion", 5);
+    }
+
+    Parcial * crearParcialConLongitudImpar() {
+      return Parcial_new("Sistemas Operativos", 8);
+    }
+
+    Parcial * crearParcialConMuchasPreguntas() {
+      return Parcial_new("Paradigmas de programacion", 6);
+    }
+
+    Parcial * crearParcialConPocasPreguntas() {
+      return Parcial_new("Sistemas Operativos", 4);
+    }
 
     it("nombre completo de un alumno") {
-      should_string(nombreCompleto(nico)) be equal to("Gomez, Nico");
+      // Arrange
+      Alumno * alumno = crearAlumno();
+      // Assert
+      should_string(nombreCompleto(alumno)) be equal to("Gomez, Nico");
+      // Liberamos espacio. Otra opción es utilizar los hooks before y after, pero
+      // cuando crece mucho el archivo es difícil de controlar el juego de datos
+      FREE(alumno);
     } end
 
     it("una persona es mayor de edad") {
-        should_bool(esMayorDeEdad(nico)) be truthy;
+      Alumno * alumno = crearAlumno();
+      should_bool(esMayorDeEdad(alumno)) be truthy;
+      FREE(alumno);
     } end
     
     it("una persona no es mayor de edad") {
-      nico->edad = 15;
-      should_bool(esMayorDeEdad(nico)) be falsey;
+      Alumno * alumno = crearAlumno();
+      alumno->edad = 15;
+      should_bool(esMayorDeEdad(alumno)) be falsey;
+      FREE(alumno);
     } end
     
     it("una persona estudiosa siempre estudia") {
-      should_bool(estudia(alumnoEstudioso, parcialConLongitudPar)) be truthy;
+      Alumno * alumnoEstudioso = crearAlumnoEstudioso();
+      Parcial * parcial = crearParcialConLongitudPar();
+      should_bool(estudia(alumnoEstudioso, parcial)) be truthy;
+      FREE(alumnoEstudioso);
     } end
 
     it("una persona hija del rigor NO estudia si el parcial tiene pocas preguntas"){
+      Alumno * alumnoHijoDelRigor = crearAlumnoHijoDelRigor();
+      Parcial * parcialConPocasPreguntas = crearParcialConPocasPreguntas();
       should_bool(estudia(alumnoHijoDelRigor, parcialConPocasPreguntas)) be falsey;
+      FREE(parcialConPocasPreguntas);
+      FREE(alumnoHijoDelRigor);
     } end
     
     it("una persona hija del rigor estudia si el parcial tiene muchas preguntas"){
+      Alumno * alumnoHijoDelRigor = crearAlumnoHijoDelRigor();
+      Parcial * parcialConMuchasPreguntas = crearParcialConMuchasPreguntas();
       should_bool(estudia(alumnoHijoDelRigor, parcialConMuchasPreguntas)) be truthy;
+      FREE(parcialConMuchasPreguntas);
+      FREE(alumnoHijoDelRigor);
     } end
 
     it("una persona cabulera estudia si el parcial tiene una cantidad par de preguntas"){
+      Alumno * alumnoCabulero = crearAlumnoCabulero();
+      Parcial * parcialConLongitudPar = crearParcialConLongitudPar();
       should_bool(estudia(alumnoCabulero, parcialConLongitudPar)) be truthy;
+      FREE(parcialConLongitudPar);
+      FREE(alumnoCabulero);
     } end
 
     it("una persona cabulera NO estudia si el parcial tiene una cantidad impar de preguntas"){
+      Alumno * alumnoCabulero = crearAlumnoCabulero();
+      Parcial * parcialConLongitudImpar = crearParcialConLongitudImpar();
       should_bool(estudia(alumnoCabulero, parcialConLongitudImpar)) be falsey;
+      FREE(parcialConLongitudImpar);
+      FREE(alumnoCabulero);
     } end
 
   } end
